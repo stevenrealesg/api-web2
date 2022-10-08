e1f457a506a8dd8dc19dcabaed2e760ee93fe406
Steven Reales <stevenreales@gmail.com>
2022-10-08 15:09:02 -0500

    Pager functionality

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,12 +5,16 @@ function List() {
 
     const [pokemons, setPokemons] = useState([])
     const [loading, setLoading] = useState(false)
+    const [offset, setOffset] = useState(0)
+    const [totalRecords, setTotalRecords] = useState(0)
+    const limit = 15
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const res = await fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=6")
+                const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`)
                 const data = await res.json()
+                setTotalRecords(data.count)
                 const promises = data.results.map(async item => {
                     const resPokemon = await fetch(item.url)
                     const dataPokemon = await resPokemon.json()
@@ -22,37 +26,52 @@ function List() {
                     }
                 })
                 setPokemons(await Promise.all(promises))
+                setLoading(false)
             } catch (error) {
                 console.error("Error al obtener la información.", error)
+                setLoading(false)
             }
         }
+        setLoading(true)
         fetchData()
-    }, [])
+    }, [offset])
 
     return (
         <>
             <div className="list-cards">
                 <h2>Lista de pokemones</h2>
                 <div className="paginator">
-                    <button className="p-back">{'< Anterior'}</button>
-                    <button className="p-next">{'Siguiente >'}</button>
+                    <button 
+                        className="p-back" 
+                        onClick={() => setOffset(prev => prev - limit)}
+                        disabled={offset === 0}
+                    >
+                        {'< Anterior'}
+                    </button>
+                    <button 
+                        className="p-next"
+                        onClick={() => setOffset(prev => prev + limit)}
+                        disabled={(offset + limit) >= totalRecords}
+                    >
+                        {'Siguiente >'}
+                    </button>
                 </div>
                 <div className="cards-container">
                     {pokemons.map(pokemon => (
-                        // <div className="grid-item">item</div>
                         <Pokemon key={pokemon.id} {...pokemon} />
                     ))}
                 </div>
                 <div className="footer">Created by Steven Reales Gonzalez</div>
             </div>
-            <div className="backdrop">
-                <div class="lds-ring">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-            </div>
+            {loading &&
+                <div className="backdrop">
+                    <div className="lds-ring">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>}
         </>
     );
 }
