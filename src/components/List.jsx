144919651e45a5e867e38e730c152285084100e0
Steven Reales <stevenreales@gmail.com>
2022-10-07 22:44:46 -0500

    Api consumption

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
 import Pokemon from "./Pokemon";
 
 function List() {
-    return ( 
+
+    const [pokemons, setPokemons] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=2")
+                const data = await res.json()
+                const promises = data.results.map(async item => {
+                    const resPokemon = await fetch(item.url)
+                    const dataPokemon = await resPokemon.json()
+                    return {
+                        id: dataPokemon.id,
+                        name: dataPokemon.name,
+                        weight: dataPokemon.weight,
+                        image: dataPokemon.sprites.other.dream_world.front_default
+                    }
+                })
+                setPokemons(await Promise.all(promises))
+            } catch (error) {
+                console.error("Error al obtener la información.", error)
+            }
+        }
+        fetchData()
+    }, [])
+
+    return (
         <div className="list-cards">
             <h2>Lista de pokemones</h2>
             <Pokemon />
